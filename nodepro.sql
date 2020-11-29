3ee79f11ba1dd12d2df9838ab516fd7849ec75e3
xishanqianxue <2496849862@qq.com>
2020-11-29 16:44:52 +0800

    now

---
+++ nodepro.sql
@@ -0,0 +1,58 @@
+-- --------------------------------------------------------
+-- 主机:                           127.0.0.1
+-- 服务器版本:                        5.5.53 - MySQL Community Server (GPL)
+-- 服务器操作系统:                      Win32
+-- HeidiSQL 版本:                  9.5.0.5220
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- 导出 nodepro 的数据库结构
+CREATE DATABASE IF NOT EXISTS `nodepro` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `nodepro`;
+
+-- 导出  表 nodepro.books 结构
+CREATE TABLE IF NOT EXISTS `books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `goodsName` varchar(50) DEFAULT NULL,
+  `goodsKind` varchar(50) DEFAULT NULL,
+  `goodsPrice` varchar(50) DEFAULT NULL,
+  `goodsID` varchar(50) DEFAULT NULL,
+  `goodsDesc` varchar(500) DEFAULT NULL,
+  `goodsImg` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+-- 数据导出被取消选择。
+-- 导出  表 nodepro.messager 结构
+CREATE TABLE IF NOT EXISTS `messager` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `pwd` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- 数据导出被取消选择。
+-- 导出  表 nodepro.user 结构
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `accountnumber` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `tel` int(11) DEFAULT NULL,
+  `autograph` varchar(50) DEFAULT NULL,
+  `jianjie` varchar(500) DEFAULT NULL,
+  `jybj` varchar(50) DEFAULT NULL,
+  `lxqq` varchar(50) DEFAULT NULL,
+  `maillbox` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- 数据导出被取消选择。
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
